9554b2d6c4fbb52cddc02ed59769fb2529c1c867
Hikikomono <enkoroberto@gmail.com>
2020-04-27 11:26:21 +0200

    backup.sh updated + before WC change

--- backup.sh
+++ backup.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 #Created by Hikikomono
-#backup.sh backs up my ~/scripts directory and saves it in /tmp
+#backup.sh backs up iy ~/scripts directory and saves it in /tmp
 #it creates a compressed tarball and filters? it using gzip
 #my /home dir was way too big - so I backup ~/scripts dir
 
 
+#function to count files in current directory
+nrFiles () {
+	echo $(ls | wc -l)	
+}
+
+#function to count amnt of directories in current directory
+nrDirectories () {
+	 echo $(ls -d | wc -l)
+	#bug -> printing name of prev func (nrFiles) wtf? -> old vers.
+}
 
 user=$USER
 date_=$(date +"%Y-%m-%d_%H:%M:%S")
@@ -13,9 +23,17 @@ date_=$(date +"%Y-%m-%d_%H:%M:%S")
 source_dir=~/scripts
 destination_dir=/tmp/scripts_${user}_${date_}.tar.gz
 
+let amt_files_before=$(nrFiles)
+
 tar -czf $destination_dir $source_dir 2> /dev/null
 
-echo Successfull Backup of: $source_dir
+let amt_files_after=$(nrFiles)
 
 
+if ((amt_files_before==amt_files_after)) 
+then
+	echo Successfull Backup of: $source_dir
+else 
+	echo An Error occured while performing the Backup
+fi
 
